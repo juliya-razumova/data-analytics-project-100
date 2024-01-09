fb0025afa68e5e16105cb3623807ef13a69c0260
juliya-razumova <juliya.razumova@yandex.ru>
2024-01-09 12:00:05 +0300

    try2

--- main.py
+++ main.py
@@ -248,7 +248,7 @@ def run_all():
     conversion.to_json('./conversion.json')
     
     # шаг 4 добавляем данные рекламы к общей базе данных
-    ads=pd.read_csv('./ads.csv')
+    ads=pd.read_csv('../ads.csv')
     ads['date']=pd.to_datetime(ads['date']).dt.date
     
     total_conversion=conversion.drop(['platform', 'conversion'], axis=1).groupby(['date_group']).agg('sum').reset_index()
